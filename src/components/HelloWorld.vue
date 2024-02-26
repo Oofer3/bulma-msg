b9ec9854084b6280489db33706e7a59300ce6907
Jaan Laskar <jaan.laskar@tptlive.ee>
2024-02-26 11:23:50 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from 'vue'
+import Message from './Message.vue';
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+  <Message header="test" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis
+            placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+            <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+            sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et
+            sem eget, facilisis sodales sem.">
+
+  </Message>
+  <Message header="test" type="warning" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis
+            placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+            <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+            sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et
+            sem eget, facilisis sodales sem.">
+
+  </Message>
+  <Message type="danger" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis
+            placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+            <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+            sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et
+            sem eget, facilisis sodales sem.">
+
+  </Message>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+    <article class="message" :class="[type !== '' ? 'is-'+type : '']">
+        <div v-if="header" class="message-header">
+            <p>{{ header }}</p>
+            <button class="delete" aria-label="delete" @click="$emit('close')"></button>
+        </div>
+        <div class="message-body">
+            {{ body }}
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    props:
+    {
+        header: String,
+        body: {
+            type: String,
+            required: true
+        },
+        type: {
+            default: "",
+            validator(value, props) {
+                // The value must match one of these strings
+                return ['', 'dark', 'danger', 'primary', 'link', 'info', 'success', 'warning'].includes(value)
+            }
+        }
+    },
+    emits: ["close"]
+}
+</script>
